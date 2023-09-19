66ef40705d246c9cce0d3289e21e19d4ca87872e
Willy <willykipchumba22@gmail.com>
2023-09-19 21:51:35 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-
 // Write your code here
+let multiply;
+let num1, num2, num3, num4;
+
+let random;
+
+let mod;
+
+let max;
+// set to an equation that will multiply the letiables num1 and num2
+num1 = 31;
+num2 = 2;
+multiply = num1 * num2;
+
+console.log(multiply);
+//Create a letiable called random that will generate a random integer greater than 0.
+random = Math.random() * 100 | 0;
+console.log(random);
+//Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+num3=9;
+num4=5;
+mod = num3 % num4;
+console.log(mod);
+//Create a variable called max that finds the highest number in a set; the value returned should be 20
+max = Math.max(5, 7, 13, 9, 20);
+console.log(max);
